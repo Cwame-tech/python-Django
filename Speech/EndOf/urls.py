1927d28f48f8f3703302269c61b4e248d481ebe9
Cwame-tech <cwameagyapong@gmail.com>
2021-02-28 23:48:13 +0000

    Initial commit

--- Speech/EndOf/urls.py
+++ Speech/EndOf/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views
-
-
+from .views import (
+    PostListView,
+)
+from .import views
 urlpatterns = [
+    
     path('',views.index, name = 'index'),
-    path('speech/', views.home, name = 'speech'),
+    path('speech/', PostListView.as_view(), name = 'speech'),
     path('about/', views.about, name = 'EndOf-about'),
     path('about/', views.about, name = 'EndOf-about'),
     path('speaker/',views.Speakview, name = 'speaker_detail'),
--- Speech/EndOf/views.py
+++ Speech/EndOf/views.py
@@ -3,17 +3,27 @@ from django.http import HttpResponse
 from .models import Speech,Speaker
 from django.views import generic
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 # Create your views here.
 
 
 def index(request):
     return render(request,'speech/welcome.html')
 
+    
+
 def home(request):
     context = {
         'posts': Speech.objects.all()
     }
     return render(request,'speech/home.html',context)
+
+class PostListView(ListView):
+    model = Speech
+    template_name = 'speech/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    # ordering = ['-date_posted']
+    paginate_by = 3
 
 def about(request):
     context = {
